375a34c4ad32966f1d59c89b596db1fa0be2f05e
Pawan Barde <pawanbbarde1998@gmail.com>
2025-08-25 20:17:46 +0530

    add_devise

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def test_email
+    user = User.new(email: 'test@example.com')  # Replace with a valid test user's email
+    UserMailer.welcome_email(user).deliver_now
+
+    # Optionally, you can set a flash message to confirm that the email has been sent
+    #flash[:notice] = 'Test email sent!'
+    #redirect_to root_path  # Redirect to a suitable path (e.g., homepage)
+  end
+end
